16af08687e159d1f84eac7776da57813b436cfb5
Abhishek Kumawat <abhikumawat621@gmail.com>
2026-09-08 14:52:50 +0530

    Support Vercel runtime port

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 // Enable Gzip/Brotli HTTP compression
 app.use(
